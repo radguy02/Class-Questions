d53097d0b902fd9daa7accd9d71f5591afe09a5c
Anish Raj <105291901+radguy02@users.noreply.github.com>
2023-10-05 21:46:06 +0530

    pascals triangle.py

--- pascals triangle.py
+++ pascals triangle.py
@@ -2,8 +2,8 @@ n = int(input('No. of rows: '))
 for i in range(1, n+1):
 	for j in range(0, n-i+1):
 		print(' ', end='')
-	C = 1								# first element is always 1
+	C = 1							# first element is always 1
 	for j in range(1, i+1):				
-		print(' ', C, sep='', end='')	# first value in a line is always 1
-		C = C * (i - j) // j			# using Binomial Coefficient
+		print(' ', C, sep='', end='')			# first value in a line is always 1
+		C = C * (i - j) // j				# using Binomial Coefficient
 	print()
