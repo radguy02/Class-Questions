c180f79566c3425af384afbe9b6788afdedc221a
radguy02 <anishraj365@gmail.com>
2024-02-23 23:04:16 +0530

    deletd the file

--- printing.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int num = 20;
-    double decimal = 1.8;
-    string name = "Anish";
-    cout << "Hi! My name is " << name << " and I am " << num <<" years old."<<endl;
-    return 0;
-}
