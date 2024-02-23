298cfe381570e4025155a5a91ca7bcfbbd0f0942
radguy02 <anishraj365@gmail.com>
2024-02-23 22:46:19 +0530

    New language to learn for me

---
+++ printing.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 20;
+    double decimal = 1.8;
+    string name = "Anish";
+    cout << "Hi! My name is " << name << " and I am " << num <<" years old."<<endl;
+    return 0;
+}
